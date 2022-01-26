c0c097152e16ed57a8f3a0089f5b7974bbc28882
bishwajit <arin17bishwa@gmail.com>
2022-01-26 18:00:43 +0530

    initial view(GET only) only for round1

--- DCPortal/dc/views.py
+++ DCPortal/dc/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .constants import Quiz1
+
 
 # Create your views here.
+
+def takeQuizView(request):
+    if request.method == 'POST':
+        pass
+    else:
+        context = {}
+        quiz1 = Quiz1()
+        context = {
+            'quiz1': quiz1.serialize()
+        }
+        return render(request, 'dc/takeQuiz.html', context=context)
