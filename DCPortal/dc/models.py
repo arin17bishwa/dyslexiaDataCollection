e5d8f02c29147677cdeba5ff5d0d722e694e422b
bishwajit <arin17bishwa@gmail.com>
2022-01-26 17:56:27 +0530

    created initial data model

--- DCPortal/dc/models.py
+++ DCPortal/dc/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Data(models.Model):
+    name = models.CharField(max_length=64, null=False, blank=False)
+    age = models.SmallIntegerField(null=False)
+    score1 = models.SmallIntegerField(default=0)
+    score2 = models.SmallIntegerField(default=0)
+    score3 = models.SmallIntegerField(default=0)
+    answer1 = models.JSONField()
+    answer2 = models.JSONField()
+    answer3 = models.JSONField()
