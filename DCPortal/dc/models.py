7a70c1eab43fabba0d9e4b9e9bd7b167fbe01e3b
bishwajit <arin17bishwa@gmail.com>
2022-02-23 02:31:58 +0530

    added model Data2

--- DCPortal/dc/models.py
+++ DCPortal/dc/models.py
@@ -10,6 +10,14 @@ def upload_location(instance, filename, **kwargs):
     return file_path
 
 
+def upload_location_older(instance, filename, **kwargs):
+    file_path = 'r4_older/images/{_id:05d}_{filename}'.format(
+        _id=instance.id,
+        filename=filename
+    )
+    return file_path
+
+
 class Data(models.Model):
     name = models.CharField(max_length=64, null=False, blank=False)
     age = models.SmallIntegerField(null=False)
@@ -36,3 +44,35 @@ class Data(models.Model):
 
     def __repr__(self):
         return self.__str__()
+
+
+class Data2(models.Model):
+    name = models.CharField(max_length=64, null=False, blank=False)
+    age = models.SmallIntegerField(null=False)
+    score1 = models.SmallIntegerField(default=0)
+    score2 = models.SmallIntegerField(default=0)
+    score3 = models.SmallIntegerField(default=0)
+    score4 = models.SmallIntegerField(default=0)
+    answer1 = models.JSONField(null=True)
+    answer2 = models.JSONField(null=True)
+    answer3 = models.JSONField(null=True)
+    image = models.FileField(
+        upload_to=upload_location_older,
+        blank=True,
+        null=True
+    )
+    submitted = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "Name:{} Age:{} ({},{},{})".format(
+            self.name,
+            self.age,
+            self.score1, self.score2, self.score3
+        )
+
+    def __repr__(self):
+        return self.__str__()
+
+    class Meta:
+        verbose_name = 'Data of older student'
+        verbose_name_plural = 'Data of older students'
